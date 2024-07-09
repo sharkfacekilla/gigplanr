281020d4a81b65855d9cbca15409f70cd212d16e
sharkfacekilla <sharkfacekilla@gmail.com>
2024-07-08 18:48:01 -0700

    added logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,18 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'black': '#0B132B',
+                'dark-blue': '#1c2541',
+                'light-blue': '#3A506B',
+                'teal': '#81FFD9',
+                'green': '#5BC0B3',
+                'white': '#FFFFFF',
+                'dark-black': '#222222',
+                'red': '#ee4266',
+                'yellow': '#ffff82',
+                'pink': '#d1b1c8'
+            }
         },
     },
 
