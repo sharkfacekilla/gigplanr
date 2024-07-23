7e4a1bdf062918db50cbdd4af3ed77ed64174370
sharkfacekilla <sharkfacekilla@gmail.com>
2024-07-22 21:16:14 -0700

    edited email css

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
+use App\User;
 use Inertia\Inertia;
+use App\Mail\VerifyEmail;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProfileController;
+use Tests\Feature\Auth\EmailVerificationTest;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -13,15 +18,39 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+ 
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+ 
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+ 
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+   
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::get('/verify-test', function () {
+    // Get a user for demo purposes
+    $user = App\Models\User::find(1);
+    return (new Illuminate\Auth\Notifications\VerifyEmail())->toMail($user);
 });
 
 require __DIR__.'/auth.php';
