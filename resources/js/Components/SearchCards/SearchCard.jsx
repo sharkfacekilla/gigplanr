f7ba006178ef7081ef8b8780963c350b178a9c9b
sharkfacekilla <sharkfacekilla@gmail.com>
2024-07-25 14:31:59 -0700

    added placeholder image

--- resources/js/Components/SearchCards/SearchCard.jsx
+++ resources/js/Components/SearchCards/SearchCard.jsx
@@ -6,9 +6,12 @@ export default function SearchCard({ item }) {
         <div className="w-full mx-auto max-w-xs sm:max-w-sm rounded-3xl overflow-hidden flex flex-col shadow-lg shadow-white/10">
             {/* Render image */}
             {imageUrl ? (
-                <img src={imageUrl} alt={title} className="h-100 w-full object-cover" />
+                <img src={imageUrl} alt={title} className="w-full object-cover h-40 sm:h-48 md:h-64 lg:h-72" />
             ) : (
-                <div className="w-full h-40 bg-gray-200 flex items-center justify-center text-gray-600">No Image</div>
+                <div className="w-full h-full bg-gray-200 flex items-center justify-center text-gray-600">
+                <img src={"https://fakeimg.pl/1920x1920?text=+No+Image&font=bebas"} alt="No Image Found."/>
+            </div>
+                
             )}
             {/* Render title */}
             <div className="p-4 flex bg-dark-black flex-col flex-1">
