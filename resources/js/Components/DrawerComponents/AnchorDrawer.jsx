ea9010fd4a84fecc2de56c4a407cc9374dc5aafb
sharkfacekilla <sharkfacekilla@gmail.com>
2024-08-01 23:46:54 -0700

    albums appaer first, and various is last.

--- resources/js/Components/DrawerComponents/AnchorDrawer.jsx
+++ resources/js/Components/DrawerComponents/AnchorDrawer.jsx
@@ -3,31 +3,45 @@ import Button from '@mui/material/Button';
 import { Link } from '@inertiajs/react';
 
 export default function AnchorDrawer({ toggleDrawer, songs, handleAlbumSelect }) {
-    const uniqueAlbums = Array.from(
-        new Map(
-            songs.data.map((song) => [
-                `${song.album}`,
-                song,
-            ])
-        ).values()
-    );
+	const albums = songs.data.filter(song => song.album);
+	const nonAlbums = songs.data.filter(song => song.album === null);
+	const uniqueAlbums = Array.from(
+		new Map(
+			albums.filter(song => song.album !== null).map((song) => [
+				song.album, song
+			])
+		).values()
+	);
+	const uniqueNonAlbums = Array.from(
+		new Map(
+			nonAlbums.map((song) => [
+				song.album, song
+			])
+		).values()
+	);
+	const sortedAlbums = [...uniqueAlbums, ...uniqueNonAlbums];
+	console.log(sortedAlbums);
     return (
         <>
       		<div className="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
         		<div className="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4">
-          			{uniqueAlbums.map((album, index) => (
-            			<div key={index} className="p-4 bg-background">
+          			{sortedAlbums.map((album, index) => (
+            			<div key={index} className="p-4 bg-blue rounded-xl shadow-xl shadow-dark-black hover:shadow-lg hover:shadow-teal move-up">
             				<div role="button" tabIndex={0} onClick={() => handleAlbumSelect(album)} className="relative w-full cursor-pointer">
                 				<div className="w-full" style={{ paddingTop: '100%', position: 'relative' }}>
                   					<img
                     					src={album.album_cover ? album.album_cover : "/img/no-img.svg"}
                     					alt="Album Cover"
-                    					className="absolute inset-0 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-80"
+                    					className="absolute inset-0 w-full h-full object-cover transition-opacity duration-300 group-hover:opacity-80 rounded-t-xl"
                   					/>
                 				</div>
                 				<span className="sr-only">View Album</span>
-                				<h3 className="text-lg font-semibold text-foreground line-clamp-2 mt-2 text-white">{album.album ? album.album : "Various/Unknown Albums"}</h3>
+								<div className="bg-dark-black pt-4 pb-4 rounded-b-xl ps-4">
+
+                				<h3 className="text-lg font-semibold text-foreground line-clamp-1 mt-2 text-white">{album.album ? album.album : "Various/Unknown Albums"}</h3>
                 				<p className="text-sm text-muted-foreground text-white mt-2">{album.artist ? album.artist : "Various/Unknown Artists"}</p>
+								</div>
+
               				</div>
             			</div>
           			))}
